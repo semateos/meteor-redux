fc38a0dca2a4ea1956e9974cf9d2c2a9f12a6f84
Sam Mateosian <sam@bigroomstudios.com>
2018-02-12 17:33:29 -0500

    making format more consistant

--- imports/api/tasks/methods.js
+++ imports/api/tasks/methods.js
@@ -2,8 +2,8 @@ import { wireMethods } from '/imports/lib/wireMethods';
 import { Tasks } from './collection';
 
 const methods = [
-
-  { name: 'upsertTask',
+  {
+    name: 'upsertTask',
     returns: 'Task',
     params: {
       _id: { type: String, optional: true },
@@ -11,39 +11,35 @@ const methods = [
       details: { type: String, optional: true },
       done: { type: Boolean, optional: true },
     },
-
     run: async ({ _id, ...item }) => {
       const result = await Tasks.upsert({ _id }, { $set: item });
       const id = (result.insertedId) ? result.insertedId : _id;
       return Tasks.findOne({ _id:id });
     },
   },
-
-  { name: 'setTaskDone',
+  {
+    name: 'setTaskDone',
     returns: 'Task',
     params: {
       _id: { type: String },
       done: { type: Boolean },
     },
-
     run: ({ _id, ...item }) => {
       Tasks.update({ _id }, { $set: item });
       return Tasks.findOne({ _id });
     },
   },
-
-  { name: 'removeTask',
+  {
+    name: 'removeTask',
     returns: Boolean,
     params: {
       _id: { type: String },
     },
-
     run: ({ _id }) => {
       const res = Tasks.remove({ _id });
       return (res);
     },
   },
-
 ];
 
 export default wireMethods(methods);
